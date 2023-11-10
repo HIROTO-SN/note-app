17a8969801a2010dfcc373eb88c579ae5c7c2029
HIROTO NAKASHIMA <hiroto4455656@gmail.com>
2023-11-10 10:53:08 +0900

    create hard-code note addtions

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
-import { useState } from 'react'
+import { useReducer, useState } from 'react'
 import './App.css';
 import Sidebar from "./components/Sidebar"
 import Main from './components/Main';
 
 function App() {
+  // const [notes, setNotes] = useState([]);
+  const [notes, notesDispatch] = useReducer((prevNotes, action) => {
+    switch(action) {
+      case 'add': {
+        const newNote = {
+          id: Math.random(),
+          title: '新しいノート',
+          content: '新しいノート内容',
+          modDate: Date.now(),
+        };
+        console.log(newNote);
+        return [...prevNotes, newNote];
+      }
+    }
+  }, []);
+
+  const onAddNote = () => {
+    notesDispatch('add');
+  };
+
   return (
     <div className='App'>
-      <Sidebar />
+      <Sidebar onAddNote={onAddNote} notes={notes}/>
       <Main />
     </div>
   )
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React from 'react';
+import "./style/Sidebar.css";
 
-const Sidebar = () => {
+const Sidebar = ({ onAddNote, notes }) => {
 	return (
-		<div>Sidebar</div>
+		<div className='app-sidebar'>
+			<div className='app-sidebar-header'>
+				<h1>ノート</h1>
+				<button onClick={onAddNote}>追加</button>
+			</div>
+			<div className="app-sidebar-notes">
+				{notes.map((note) => {
+					return (
+						<div className="app-sidebar-note" key={note.id}>
+							<div className="sidebar-note-title">
+								<strong>{note.title}</strong>
+								<button>削除</button>
+							</div>
+							<p>{note.content}</p>
+							<small>最後の修正日:{new Date(note.modDate).toLocaleDateString("ja-JP")}</small>
+						</div>
+					)
+				})}
+			</div>
+		</div>
 	)
 }
 
